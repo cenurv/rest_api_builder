a2976929e113fc1da9596683ea859d2124397f94
Joseph W. Lindley <josephl@tazworks.com>
2017-02-05 17:07:33 -0700

    Added more features to auto_api.

--- lib/auto_api.ex
+++ lib/auto_api.ex
@@ -5,73 +5,127 @@ defmodule AutoApi do
 
   defmacro __using__(opts) do
     resource_name = to_string Keyword.get(opts, :name, nil)
+    activate = Keyword.get opts, :activate, nil
 
-    quote do
-      use Plug.Router
+    output =
+      quote do
+        use Plug.Router
 
-      import AutoApi
+        import AutoApi
 
-      plug :match
-      plug :preload_plug
-      plug :dispatch
+        plug :match
+        plug :preload_plug
+        plug :dispatch
 
-      @resource_path unquote(resource_name)
+        @resource_path unquote(resource_name)
 
-      defp send_response(conn, status, resource) do
-        conn
-        |> Plug.Conn.put_resp_content_type("application/json")
-        |> Plug.Conn.send_resp(status, Poison.encode!(resource))
-      end
-
-      defmacro route_to(prepend_path, module_path) do
-        path = "#{prepend_path}/#{@resource_path}"
-        quote do
-          forward unquote(path), to: unquote(module_path)
+        defp send_response(conn, status, resource) do
+          conn
+          |> Plug.Conn.put_resp_content_type("application/json")
+          |> Plug.Conn.send_resp(status, Poison.encode!(resource))
         end
+
+        defmacro route_to(prepend_path, module_path) do
+          path = "#{prepend_path}/#{@resource_path}"
+          quote do
+            forward unquote(path), to: unquote(module_path)
+          end
+        end
+
+        def preload_plug(%Plug.Conn{path_params: %{"id" => id}} = conn, _opts) do
+          preload conn
+        end
+        def preload_plug(conn, _opts) do
+          conn
+        end
+
+        defp __not_ready__(conn) do
+          conn
+          |> put_resp_content_type("text/plain")
+          |> send_resp(501, "Not yet implemented.")
+        end
+
+        def preload(conn), do: conn
+        def index(conn), do: __not_ready__ conn
+        def show(conn), do: __not_ready__ conn
+        def create(conn), do: __not_ready__ conn
+        def update(conn), do: __not_ready__ conn
+        def delete(conn), do: __not_ready__ conn
+
+        defoverridable [index: 1, show: 1, create: 1, update: 1, delete: 1, preload: 1]
       end
 
-      def preload_plug(%Plug.Conn{path_params: %{"id" => id}} = conn, _opts) do
-        preload conn
-      end
-      def preload_plug(conn, _opts) do
-        conn
-      end
+    if activate do
+      activate_output =
+        quote do
+          activate unquote(activate)
+        end
 
-      defp __not_ready__(conn) do
-        conn
-        |> put_resp_content_type("text/plain")
-        |> send_resp(501, "Not yet implemented.")
-      end
+      [output, activate_output]
+    else
+      output
+    end
+  end
 
-      def preload(conn), do: conn
-      def index(conn), do: __not_ready__ conn
-      def show(conn), do: __not_ready__ conn
-      def create(conn), do: __not_ready__ conn
-      def update(conn), do: __not_ready__ conn
-      def delete(conn), do: __not_ready__ conn
+  @doc """
+  Generates the router matching for the following actions:
 
-      defoverridable [index: 1, show: 1, create: 1, update: 1, delete: 1, preload: 1]
-
+  * index
+  * show
+  * create
+  * update
+  * delete
+  """
+  defmacro activate(:all) do
+    quote do
+      activate [:index, :show, :create, :update, :delete]
+    end
+  end
+  defmacro activate(:index) do
+    quote do
       get "/" do
         index var!(conn)
       end
-
+    end
+  end
+  defmacro activate(:show) do
+    quote do
       get "/:id" do
         show var!(conn)
       end
-
+    end
+  end
+  defmacro activate(:create) do
+    quote do
       post "/" do
         create var!(conn)
       end
-
+    end
+  end
+  defmacro activate(:update) do
+    quote do
       put "/:id" do
         update var!(conn)
       end
 
+      patch "/:id" do
+        update var!(conn)
+      end
+    end
+  end
+  defmacro activate(:delete) do
+    quote do
       delete "/:id" do
         delete var!(conn)
       end
     end
+  end
+  defmacro activate(actions) when is_list actions do
+    for action <- actions do
+      quote do
+        activate unquote(action)
+      end
+    end    
   end
 
   defmacro provider(provider_module, opts) do
@@ -96,10 +150,43 @@ defmodule AutoApi do
   end
 
   defmacro feature(name, do: block) do
-    path = "/:id/#{name}"
     quote do
-      post unquote(path) do
+      feature unquote(name), only: [:post] do
         unquote(block)
+      end
+    end
+  end
+
+  defmacro feature(name, opts, [do: block]) do
+    path = "/:id/#{name}"
+    only = Keyword.get opts, :only
+
+    for method <- only do
+      quote do
+        match unquote(path), via: unquote(method) do
+          unquote(block)
+        end
+      end
+    end
+  end
+
+  defmacro group_feature(name, do: block) do
+    quote do
+      group_feature unquote(name), only: [:post] do
+        unquote(block)
+      end
+    end
+  end
+
+  defmacro group_feature(name, opts, [do: block]) do
+    path = "/#{name}"
+    only = Keyword.get opts, :only
+
+    for method <- only do
+      quote do
+        match unquote(path), via: unquote(method) do
+          unquote(block)
+        end
       end
     end
   end
--- lib/ecto_schema_store/api_provider.ex
+++ lib/ecto_schema_store/api_provider.ex
@@ -13,6 +13,33 @@ defmodule EctoSchemaStore.ApiProvider do
       end
 
     quote do
+      def store, do: unquote(store)
+
+      defp whitelist(models) when is_list models do
+        Enum.map models, fn(model) -> whitelist model end
+      end
+      defp whitelist(model) do
+        keys = EctoSchemaStore.Utils.keys unquote(store).schema
+        Map.take model, keys
+      end
+
+      defp fetch_all(%Plug.Conn{assigns: %{current: parent}} = conn) do
+        parent_field = unquote(parent_field)
+
+        if parent && parent_field do
+          query =
+            []
+            |> Keyword.put(parent_field, parent.id)
+
+          unquote(store).all query 
+        else
+          unquote(store).all
+        end
+      end
+      defp fetch_all(conn) do
+        unquote(store).all
+      end
+
       def preload(%Plug.Conn{path_params: %{"id" => id}, assigns: assigns} = conn) do
         parent_field = unquote(parent_field)
 
@@ -47,10 +74,15 @@ defmodule EctoSchemaStore.ApiProvider do
       def show(%Plug.Conn{assigns: %{current: model}} = conn) do
         case conn.assigns.current do
           nil -> send_response conn, 404, "Not Found"
-          model -> send_response conn, 200, unquote(store).to_map(model)
+          model -> send_response conn, 200, whitelist(unquote(store).to_map(model))
         end
       end
       def show(conn), do: send_response conn, 404, "Not Found"
+
+      def index(conn) do
+        records = fetch_all conn
+        send_response conn, 200, whitelist(unquote(store).to_map(records))
+      end
     end
   end
 end
